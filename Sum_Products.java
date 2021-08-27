4a25d2743d20df885c8106cf5e58ea6d667983c3
koustavmoitra <78336764+koustavmoitra@users.noreply.github.com>
2021-08-27 18:35:54 +0530

    Update Sum_Products.java

--- Sum_Products.java
+++ Sum_Products.java
@@ -6,7 +6,7 @@ public class Sum_Products {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("enter how many numbers of sum and product you want to calculate...");
-        int i, product = 1,sum =0;
+        int i, product=1,sum =0;
         int limit = sc.nextInt();
 
          for ( i = 0; i<limit; i++) {
@@ -20,7 +20,7 @@ public class Sum_Products {
         System.out.println("total product = " + product);
 
         int result = product - sum;
-        System.out.println("the difference between the product of its digits and the sum of its digits " + result);
+        System.out.println("the difference between the product of its digits and the sum of its digits = " + result);
 
         }
 
